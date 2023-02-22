a3c8b7408ec91666a750a8b410a9a2dbb55aa375
Urvil Patel <patelurvil38@gmail.com>
2023-02-21 21:52:01 -0500

    refactor plotting related functions in seperate file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 N_TERM=11
 # NOTE: the precision of the result will depend on the number of iterations used in the series,
 # the higher the number of terms, the more accurate the result will be but the execution time
@@ -68,26 +65,3 @@ radius = int(input("please provide the radius: "))
 # l = 2R(1 – cos(α/2))
 l = 2 * radius * (1 - cos(a/2,N_TERM))
 print("l: ", l)
-
-TERM=N_TERM
-fn=cos
-fnn=np.cos
-angles = np.arange(-2*np.pi,2*np.pi,0.1)
-p_fn = fnn(angles)
-
-fig, ax = plt.subplots()
-ax.plot(angles,p_fn)
-
-# add lines for between 1 and 6 terms in the Taylor Series
-for i in range(1,TERM+1):
-    t_fn = [fn(angle,i) for angle in angles]
-    ax.plot(angles,t_fn,linestyle='dotted')
-
-ax.set_ylim([-7,4])
-
-# set up legend
-legend_lst = ['cos() function']
-for i in range(1,TERM+1):
-    legend_lst.append(f'{i} terms')
-ax.legend(legend_lst, loc='best')
-plt.show()
---
+++ src/plot.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from main import cos
+
+TERM=11
+fn=cos
+fnn=np.cos
+angles = np.arange(-2*np.pi,2*np.pi,0.1)
+p_fn = fnn(angles)
+
+fig, ax = plt.subplots()
+ax.plot(angles,p_fn)
+
+# add lines for between 1 and 6 terms in the Taylor Series
+for i in range(1,TERM+1):
+    t_fn = [fn(angle,i) for angle in angles]
+    ax.plot(angles,t_fn,linestyle='dotted')
+
+ax.set_ylim([-7,4])
+
+# set up legend
+legend_lst = ['cos() function']
+for i in range(1,TERM+1):
+    legend_lst.append(f'{i} terms')
+ax.legend(legend_lst, loc='best')
+plt.show()
