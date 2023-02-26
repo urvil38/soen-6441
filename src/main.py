8a665f003c7f2f1c57b9eabe33f9412ae66d2865
Urvil Patel <patelurvil38@gmail.com>
2023-02-26 17:42:16 -0500

    refector code

--- src/main.py
+++ src/main.py
@@ -1,47 +1,10 @@
-N_TERM=11
-# NOTE: the precision of the result will depend on the number of iterations used in the series,
-# the higher the number of terms, the more accurate the result will be but the execution time
-# will increase as well.
-# computing the approximate value of pi using Leibniz formula.
-def value_of_pi(n_terms):
-    val = 0
-    sign = 1
-    for i in range(n_terms):
-        val += sign / (2 * i + 1)
-        sign = -sign
-    return val * 4
-
-# using 100_000 iterations
-# more iterations will reduce the error rate.
-PI = value_of_pi(100_000)
-
-def factorial(n):
-    result = 1
-    for i in range(1, n+1):
-        result *= i
-    return result
-
-# computing sin function using Taylor series expansion.
-# https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
-def sin(x,n):
-    result = 0
-    for i in range(n):
-        result += ((-1)**i)*(x**(2*i+1))/(factorial(2*i+1))
-    return result
-
-# computing cos function using Taylor series expansion.
-# https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
-def cos(x,n):
-    result = 0
-    for i in range(n):
-        result += ((-1)**i)*(x**(2*i))/(factorial(2*i))
-    return result
+from libmath import cos, sin, PI
 
 def func(alpha):
-    return alpha - sin(alpha,N_TERM) - PI/2
+    return alpha - sin(alpha) - PI/2
 
 def func_derivative(alpha):
-    return 1 - cos(alpha,N_TERM)
+    return 1 - cos(alpha)
 
 # The equation "a – sin(a) = π/2" is a transcendental equation,
 # which means that there is no algebraic solution for a in terms of elementary functions.
@@ -59,9 +22,9 @@ print("initial_guess: ", INITIAL_CASE)
 a = compute_alpha(INITIAL_CASE)
 print("pi: ", PI)
 print("a: ", a)
-print("error: {:.17f}".format(a - sin(a,N_TERM) - PI/2))
+print("error: {:.17f}".format(a - sin(a) - PI/2))
 radius = int(input("please provide the radius: "))
 
 # l = 2R(1 – cos(α/2))
-l = 2 * radius * (1 - cos(a/2,N_TERM))
+l = 2 * radius * (1 - cos(a/2))
 print("l: ", l)
