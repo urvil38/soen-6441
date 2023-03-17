47eb3f75300826b2cc6554e40740436a04171375
Urvil Patel <patelurvil38@gmail.com>
2023-03-16 21:10:58 -0400

    add output type to control the way program should print the response

--- src/main.py
+++ src/main.py
@@ -59,13 +59,15 @@ def compute_length(radius: float, alpha: float) -> float:
     return 2 * radius * (1 - cos(alpha/2))
 
 
-def interactive(alpha: float) -> None:
+def interactive(alpha: float, intital_guess: float, output_type: str) -> None:
     """
     A function that prompts the user to input a radius and calculates the corresponding length
     based on a given value of alpha.
 
     Args:
         alpha (float): The value of alpha to use in the length calculation.
+        initial_guess (float): A numeric value representing the initial guess for alpha.
+        output_type (str): Type of output response. Value can be xml or csv or None.
 
     Returns:
         None
@@ -80,30 +82,40 @@ def interactive(alpha: float) -> None:
     # l = 2R(1 – cos(α/2))
     length = compute_length(radius, alpha)
 
-    print('alpha:  {}'.format(alpha))
-    print('radius: {}'.format(radius))
-    print('length: {}'.format(length))
+    record = [(radius, length)]
+    if output_type == "csv":
+        print(generate_csv_response(alpha, record), end='')
+    elif output_type == "xml":
+        print(generate_xml_response(intital_guess, alpha, record), end='')
+    else:
+        print('alpha:  {}'.format(alpha))
+        print('radius: {}'.format(radius))
+        print('length: {}'.format(length))
 
 
 if __name__ == "__main__":
     # starting with initial guess of 1.
     INITIAL_GUESS = 1
+    argv = sys.argv
 
     alpha = compute_alpha(INITIAL_GUESS)
     if alpha is None:
         print("cannot compute value of alpha")
         sys.exit(1)
 
-    if len(sys.argv) > 2 and sys.argv[1] == "generate":
+    if len(argv) > 2 and argv[1] == "generate":
         R = [12, 14, 17, 23, 18, 29, 34, 38, 42, 41]
 
         records = []
         for radius in R:
             records.append((radius, compute_length(radius, alpha)))
 
-        if sys.argv[2] == "xml":
+        if argv[2] == "xml":
             print(generate_xml_response(INITIAL_GUESS, alpha, records), end='')
-        elif sys.argv[2] == "csv":
+        elif argv[2] == "csv":
             print(generate_csv_response(alpha, records), end='')
     else:
-        interactive(alpha)
+        OUTPUT_TYPE = None
+        if len(argv) > 1:
+            OUTPUT_TYPE = argv[1]
+        interactive(alpha, INITIAL_GUESS, OUTPUT_TYPE)
