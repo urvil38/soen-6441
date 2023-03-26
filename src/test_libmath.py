2b50b95ad0a828afd6c21da08af6e6a08ed6814c
AAA530 <dhairyapatelad@gmail.com>
2023-03-26 19:56:14 -0400

    added unittest

---
+++ src/test_libmath.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from libmath import sin, cos, PI, factorial
+import math
+from root_approx import newton_method
+from main import func, func_derivative
+
+ACCEPTABLE_ERROR = 0.0001
+a = 2.309878472457841
+INITIAL_CASE = 1
+
+
+def test_libmath_sin():
+    assert abs(1-sin(PI/2)) < ACCEPTABLE_ERROR
+    assert abs(0.5-sin(PI/6)) < ACCEPTABLE_ERROR
+
+
+def test_libmath_cos():
+    assert abs(0-cos(PI/2)) < ACCEPTABLE_ERROR
+    assert abs(0.5-cos(PI/3)) < ACCEPTABLE_ERROR
+
+
+def test_libmath_pi():
+    assert abs(math.pi-PI) < ACCEPTABLE_ERROR
+
+
+def test_libmath_factorial():
+    assert factorial(4) == 24
+    assert factorial(6) == 720
+
+
+def test_newton_method():
+    assert abs(a - newton_method(func, func_derivative,
+               INITIAL_CASE)) < ACCEPTABLE_ERROR
