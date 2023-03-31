3fe6a020d77a4898ce3934303ae50e25437ca57a
AAA530 <dhairyapatelad@gmail.com>
2023-03-31 01:36:40 -0400

    upgraded tests

---
+++ src/test_encoder.py
@@ -0,0 +1,86 @@
+import csv
+from io import StringIO
+import xml.etree.ElementTree as ET
+import unittest
+from typing import List, Tuple
+from encoder import generate_xml_response, generate_csv_response
+
+
+class TestGenerateXMLResponse(unittest.TestCase):
+
+    def test_generate_xml_response(self):
+
+        # Set up test data
+        initial_guess = 1.0
+        alpha = 0.5
+        records = [(2.0, 3.0), (4.0, 5.0), (6.0, 7.0)]
+
+        # Call the function
+        xml_str = generate_xml_response(initial_guess, alpha, records)
+
+        # Parse the XML string to an ElementTree object
+        root = ET.fromstring(xml_str)
+
+        # Assert the root element is named "result"
+        self.assertEqual(root.tag, "result")
+
+        # Assert there is one "input" element
+        input_elems = root.findall("./input")
+        self.assertEqual(len(input_elems), 1)
+
+        # Assert the "alpha" element is correct
+        alpha_elems = root.findall("./input/alpha")
+        self.assertEqual(len(alpha_elems), 1)
+        self.assertEqual(alpha_elems[0].get(
+            "initial_guess"), str(initial_guess))
+        self.assertEqual(alpha_elems[0].text, str(alpha))
+
+        # Assert there is one "output" element
+        output_elems = root.findall("./output")
+        self.assertEqual(len(output_elems), 1)
+
+        # Assert there is one "record" element for each record
+        record_elems = root.findall("./output/record")
+        self.assertEqual(len(record_elems), len(records))
+
+        # Assert each "record" element has a "radius" and "length" element with the correct values
+        for i, record_elem in enumerate(record_elems):
+            radius = records[i][0]
+            length = records[i][1]
+            radius_elem = record_elem.find("./radius")
+            length_elem = record_elem.find("./length")
+            self.assertEqual(float(radius_elem.text), radius)
+            self.assertEqual(float(length_elem.text), length)
+
+
+class TestGenerateCSVResponse(unittest.TestCase):
+
+    def test_generate_csv_response(self):
+
+        # Set up test data
+        alpha = 0.5
+        records = [(2.0, 3.0), (4.0, 5.0), (6.0, 7.0)]
+
+        # Call the function
+        csv_str = generate_csv_response(alpha, records)
+
+        # Parse the CSV string into a list of lists
+        rows = [row for row in csv.reader(StringIO(csv_str))]
+
+        # Assert the CSV header is correct
+        self.assertEqual(rows[0], ['alpha', 'radius', 'length'])
+
+        # Assert there is one row for each record, plus the header row
+        self.assertEqual(len(rows), len(records) + 1)
+
+        # Assert each row has the correct values
+        for i, row in enumerate(rows[1:]):
+            radius = records[i][0]
+            length = records[i][1]
+            self.assertEqual(float(row[0]), alpha)
+            self.assertEqual(float(row[1]), radius)
+            self.assertEqual(float(row[2]), length)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/test_libmath.py
+++ src/test_libmath.py
@@ -1,33 +1,42 @@
-from unittest import TestCase
-from libmath import sin, cos, PI, factorial
+import unittest
 import math
 from root_approx import newton_method
 from main import func, func_derivative
+from libmath import sin, cos, value_of_pi, factorial
 
 ACCEPTABLE_ERROR = 0.0001
 a = 2.309878472457841
 INITIAL_CASE = 1
 
 
-def test_libmath_sin():
-    assert abs(1-sin(PI/2)) < ACCEPTABLE_ERROR
-    assert abs(0.5-sin(PI/6)) < ACCEPTABLE_ERROR
+class TestMathFunctions(unittest.TestCase):
+
+    def test_sin(self):
+        # Test sin function against math.sin for various values of x
+        self.assertAlmostEqual(sin(0), math.sin(0))
+        self.assertAlmostEqual(sin(math.pi/2), math.sin(math.pi/2))
+        self.assertAlmostEqual(sin(math.pi), math.sin(math.pi))
+
+    def test_cos(self):
+        # Test cos function against math.cos for various values of x
+        self.assertAlmostEqual(cos(0), math.cos(0))
+        self.assertAlmostEqual(cos(math.pi/2), math.cos(math.pi/2))
+        self.assertAlmostEqual(cos(math.pi), math.cos(math.pi))
+
+    def test_value_of_pi(self):
+        # Test value_of_pi function against math.pi for various number of terms
+        self.assertAlmostEqual(value_of_pi(1000), math.pi, places=2)
+
+    def test_factorial(self):
+        # Test factorial function for various values of n
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(5), 120)
+
+    def test_newton_method(self):
+        assert abs(a - newton_method(func, func_derivative,
+                   INITIAL_CASE)) < ACCEPTABLE_ERROR
 
 
-def test_libmath_cos():
-    assert abs(0-cos(PI/2)) < ACCEPTABLE_ERROR
-    assert abs(0.5-cos(PI/3)) < ACCEPTABLE_ERROR
-
-
-def test_libmath_pi():
-    assert abs(math.pi-PI) < ACCEPTABLE_ERROR
-
-
-def test_libmath_factorial():
-    assert factorial(4) == 24
-    assert factorial(6) == 720
-
-
-def test_newton_method():
-    assert abs(a - newton_method(func, func_derivative,
-               INITIAL_CASE)) < ACCEPTABLE_ERROR
+if __name__ == '__main__':
+    unittest.main()
